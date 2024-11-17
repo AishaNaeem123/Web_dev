0133f4928b9015efa3f7ee4f34fca669c669daef
TajiyaDaarain <i212588@nu.edu.pk>
2024-11-17 16:30:15 +0500

    final

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,10 +1,14 @@
 import React    from "react";
 import Navbar from "../NavBar/NavBar";
+import SearchBar from "../SearchBar/SearchBar";
 
 class Home extends React.Component {
   render() {
     return (
+      <div>
       <Navbar></Navbar>
+      <SearchBar></SearchBar>
+      </div>
     )
   }
 }
--- src/Components/Page404/Page404.js
+++ src/Components/Page404/Page404.js
@@ -1,9 +1,20 @@
 import React    from "react";
-import template from "./Page404.jsx";
+import { Link } from 'react-router-dom';
+import "./Page404.css";
 
 class Page404 extends React.Component {
   render() {
-    return template.call(this);
+    return(
+      <div className='ErrorMsg'>
+      <h1>Error 404</h1>
+          <div className='heading'>
+            This site can't be reached
+          </div>    
+          <div className='content'>
+              Server's IP Address could not be found
+          </div>
+      </div>
+    )
   }
 }
 
